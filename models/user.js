48ae5eaf287f35a50a3a049e067195bf31f73c80
Lestyn Brooks <lbrooks@ltu.edu>
2021-03-02 14:05:57 -0500

    ESLint & Environment Variables
    ESLint was disabled for whatever reason, so I fixed that and then got the environment variable for the URI working.

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const Mongoose = require('mongoose');
 
-module.exports = Mongoose.model('User', new Mongoose.schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    age: { type: Number, required: true, min: 0 },
-    ssn: { type: String, required: true, unique: true },
-    address: {type: String, required: false },
-    phoneNumber: { type: String, required: false }
+module.exports = Mongoose.model('User', new Mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true, min: 0 },
+  ssn: {
+    type: String, required: true, unique: true, min: 11, max: 11,
+  },
+  address: { type: String, required: false },
+  phoneNumber: {
+    type: String, required: false, min: 12, max: 12,
+  },
 }));
